32b68496f7cb7e2edc636cf2b3ca1feeaafceed2
rezathriii <taheri.reza94@gmail.com>
2025-01-16 23:00:04 +0100

    complete idf2fmu

--- vbms/app.py
+++ vbms/app.py
@@ -1,9 +1,16 @@
 from fmpy import simulate_fmu
+from fmpy import read_model_description, instantiate_fmu
 
-# Simulate FMU
-result = simulate_fmu('../idfToFMU/optimized.fmu', start_time=0, stop_time=86400, step_size=3600,
-                      output=['Zone Mean Air Temperature'])
+# Read the model description
+model_description = read_model_description('./modelDescription.xml')
+print(model_description.modelVariables)
 
-# Extract Results
-for time, temperature in zip(result['time'], result['Zone Mean Air Temperature']):
-    print(f"Time: {time}, Temperature: {temperature}")
+# Try instantiating
+instance = instantiate_fmu('/root/idf2fmu', model_description)
+instance.terminate()
+#
+# result = simulate_fmu('./opt_model.fmu', start_time=0, stop_time=86400, step_size=3600,
+#                       output=['Zone Mean Air Temperature'])
+#
+# for time, temperature in zip(result['time'], result['Zone Mean Air Temperature']):
+#     print(f"Time: {time}, Temperature: {temperature}")
